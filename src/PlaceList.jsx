8780277c1f9a15e87862de7e8e49b8ad7d8c34bb
Rangatin <jumsts@gmail.com>
2024-05-31 19:44:03 +0200

    Try setup API gateway

--- src/PlaceList.jsx
+++ src/PlaceList.jsx
@@ -5,16 +5,37 @@ import './PlaceList.css';
 import 'leaflet/dist/leaflet.css';
 
 const PlaceList = ({ places, onRateClick }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://juedt3by5i.execute-api.eu-north-1.amazonaws.com/dep/places`, {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+          },
+          queryStringParameters: { // Use params for query string parameters
+            TableName: "Places", // Adjust if your Lambda expects a different name
+          },
+        });
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
-    <div className="container m-4">
-      <div className="row row-cols-1 row-cols-md-2 
-                    row-cols-lg-3 g-4">
-        {places.map((place, index) => (
-          <div key={place.id} className="col-lg-4 col-md-6 col-sm-12">
-            <PlaceCard place={place} rank={index + 1} onRateClick={onRateClick} />
-          </div>
-        ))}
-      </div>
+    <div>
+      <p> DADA</p>
+      {data ? (
+        <p>{data}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
